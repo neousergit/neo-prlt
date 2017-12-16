27522b118c27b06898f489e42f9ff1227b607729
Neobase <j0u135.uam@gmail.com>
2017-12-16 18:23:46 +0000

    Creación de módulo para tcpServer y su configuracion

--- app.js
+++ app.js
@@ -9,40 +9,7 @@ var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
-
-var net = require('net');
-
-var server = net.createServer();  
-server.on('connection', handleConnection);
-
-server.listen(13366, "0.0.0.0", function() {  
-  console.log('server listening to %j', server.address());
-});
-
-/*server.listen(1337, '127.0.0.1');*/
-
-
-function handleConnection(conn) {  
-  var remoteAddress = conn.remoteAddress + ':' + conn.remotePort;
-  console.log('new client connection from %s', remoteAddress);
-
-  conn.on('data', onConnData);
-  conn.once('close', onConnClose);
-  conn.on('error', onConnError);
-
-  function onConnData(d) {
-    console.log('connection data from %s: %j', remoteAddress, d);
-    conn.write(d);
-  }
-
-  function onConnClose() {
-    console.log('connection from %s closed', remoteAddress);
-  }
-
-  function onConnError(err) {
-    console.log('Connection %s error: %s', remoteAddress, err.message);
-  }
-}
+var tcpServer = require('./app_modules/tcpServer');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
