43b5a719d48ee3caa8f9ba4fb8e42196e842b75b
Neobase <j0u135.uam@gmail.com>
2018-01-02 04:52:49 +0000

    Fix at general services

--- services/generalServices.js
+++ services/generalServices.js
@@ -1,15 +1,13 @@
 var fs = require("fs");
 
-module.exports = function(deviceStatusDAO){
-    return {
-        readFile: function(path, callback){
-            fs = require('fs');
-            fs.readFile(path, 'utf8', function (err,data) {
-                if (err) {
-                    return callback(err);
-                }
-                callback(null, data);
-            });
-        },
-    }
+module.exports = {
+    readFile: function(path, callback){
+        fs = require('fs');
+        fs.readFile(path, 'utf8', function (err,data) {
+            if (err) {
+                return callback(err);
+            }
+            callback(null, data);
+        });
+    },
 }
